9baf0caa51da953e6e7decfac40855ee6591ddfe
Micha Niskin <asdf@ubergibson.com>
2013-12-13 10:35:49 -0500

    bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject tailrecursion/boot.core "1.0.0"
+(defproject tailrecursion/boot.core "1.1.0"
   :description  "A dependency setup/build tool for Clojure."
   :url          "https://github.com/tailrecursion/boot.core"
   :license      {:name  "Eclipse Public License"
                  :url   "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure       "1.5.1"]
-                 [me.raynes/conch           "0.5.0"]
+                 [me.raynes/conch           "0.5.0" :exclusions [org.clojure/clojure]]
                  [com.cemerick/pomegranate  "0.2.0" :exclusions [org.clojure/clojure]]
                  [digest                    "1.4.3" :exclusions [org.clojure/clojure]]]
   :aot          :all
